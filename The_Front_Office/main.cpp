0c6efd7ad6aa9ce6d21a8509cf7912ed8eca5e64
Matthew Romano <matbat2205@gmail.com>
2025-04-28 18:34:37 -0400

    Add project files.

---
+++ The_Front_Office/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// The Front Office
+// Developed By: Matthew Romano
+// Created April 28th, 2025
+
+int main(void) {
+
+	std::cout << "Hello World!" << std::endl;
+
+	return 0;
+}
